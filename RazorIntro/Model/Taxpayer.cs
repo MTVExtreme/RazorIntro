187530b1fbe67917a085c0ae71c68461d51accf2
Tyler Vermillion <mtvextreme1998@yahoo.com>
2017-04-04 16:17:23 -0400

    Created Never Ending Tax Form

---
+++ RazorIntro/Model/Taxpayer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorIntro.Model
+{
+    public class Taxpayer
+    {
+        public int TaxPayerID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string SSN { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int Zip { get; set; }
+
+
+    }
+}
